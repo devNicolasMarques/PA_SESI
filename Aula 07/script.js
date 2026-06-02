a9062699dd81621aae09cb4b19dfb0808056cc90
nicolas marques <nimarques1207@gmail.com>
2026-06-02 13:19:40 -0300

    Aula 07 - Apenas a soma

---
+++ Aula 07/script.js
@@ -0,0 +1,10 @@
+function exibirNome(){
+    let nome = document.querySelector('#nome').value
+    alert(nome)
+}
+
+function somar(){
+    let valA = Number(document.querySelector('#valorA').value)
+    let valB = Number(document.querySelector('#valorB').value)
+    alert(valA + valB)
+}
