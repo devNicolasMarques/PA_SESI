8a941e435aa952b9043ec52cdce5d55a22963e60
nicolas marques <nimarques1207@gmail.com>
2026-06-24 07:45:55 -0300

    aula 7

--- Aula 07/script.js
+++ Aula 07/script.js
@@ -1,24 +1,27 @@
+let resultado = document.querySelector('#resultado')
+
 function exibirNome(){
     let nome = document.querySelector('#nome').value
-    alert(nome)
+    let sobrenome = document.querySelector('#sobrenome').value
+    return resultado.innerHTML = `Olá ${nome} ${sobrenome}`
 }
 
 function somar(){
     let valA = Number(document.querySelector('#valorA').value)
     let valB = Number(document.querySelector('#valorB').value)
-    alert(valA + valB)
+    return resultado.innerHTML = valA + valB
 }
 
 function subtrair(){
     let valA = Number(document.querySelector('#valorA').value)
     let valB = Number(document.querySelector('#valorB').value)
-    alert(valA - valB)
+    return resultado.innerHTML = valA - valB
 }
 
 function multiplicar(){
     let valA = Number(document.querySelector('#valorA').value)
     let valB = Number(document.querySelector('#valorB').value)
-    alert(valA * valB)
+    return resultado.innerHTML = 
 }
 
 function dividir(){
