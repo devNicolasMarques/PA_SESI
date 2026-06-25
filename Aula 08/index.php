b63aebf3f18ac0903c652a0626a5ce019bf795f0
nicolas marques <nimarques1207@gmail.com>
2026-06-25 07:51:19 -0300

    Aula 08

---
+++ Aula 08/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="cadastro.php" method="post">
+        <input type="text" name="nome" placeholder="Digite seu nome...">
+        <input type="text" name="email" placeholder="Digite seu email...">
+        <input type="text" name="senha" placeholder="Digite sua senha...">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
